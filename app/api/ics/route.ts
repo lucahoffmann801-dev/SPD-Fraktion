d13bfc44872e616843124879667156d1bbf92f6d
lucahoffmann801-dev <luca.hoffmann801@gmail.com>
2026-06-15 00:21:48 +0200

    Delete app directory

--- app/api/ics/route.ts
+++
@@ -1,7 +0,0 @@
-import { getPortalData } from "@/lib/supabase";
-import { toIcs } from "@/lib/ical";
-
-export async function GET() {
-  const data = await getPortalData();
-  return new Response(toIcs(data.events ?? []));
-}
